989dda086f7cc13bb2cac07971adf143eb5d47a6
Your Name <k.bristi057@gmail.com>
2025-05-01 19:29:46 +0600

    added the cpp and js files on stack dynamic

---
+++ Code/Stack Dynamic/StackDynamic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+    int arr[n]; 
+    for(int i = 0; i < n; i++) {
+        arr[i] = i + 1;
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
---
+++ Code/Stack Dynamic/StackDynamic.js
@@ -0,0 +1,6 @@
+let n = 5;
+let arr = new Array(n);
+for (let i = 0; i < n; i++) {
+  arr[i] = i + 1;
+}
+console.log(arr);
