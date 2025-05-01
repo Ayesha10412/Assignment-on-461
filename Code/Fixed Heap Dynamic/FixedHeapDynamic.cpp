a98fa0ba7d3ba752de516772edcc7d2be2f80917
Your Name <k.bristi057@gmail.com>
2025-05-01 19:38:33 +0600

    added the cpp and js files on fixed heap dynamic

---
+++ Code/Fixed Heap Dynamic/FixedHeapDynamic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int* arr = new int[5]; 
+    for(int i = 0; i < 5; i++) {
+        arr[i] = i + 1;
+        cout << arr[i] << " ";
+    }
+    delete[] arr;
+    return 0;
+}
---
+++ Code/Fixed Heap Dynamic/FixedHeapDynamic.js
@@ -0,0 +1,5 @@
+let arr = new Array(5);
+for (let i = 0; i < 5; i++) {
+  arr[i] = i + 1;
+}
+console.log(arr);
